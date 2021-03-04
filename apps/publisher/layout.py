547b605ed6b78d61c6d58c5a554f5d07d6da3166
Rodrigo Mariano <rodmariano13@gmail.com>
2021-03-04 16:54:22 -0300

    feat: add base form

--- apps/publisher/layout.py
+++ apps/publisher/layout.py
@@ -1,5 +1,7 @@
-from dash_core_components import Input as dcc_Input
-from dash_html_components import Div, H1, H3, P
+from datetime import date
+
+import dash_core_components as dcc
+import dash_html_components as html
 from dash_table import DataTable
 from pandas import DataFrame
 
@@ -37,62 +39,132 @@ information_data = [
 df_information = DataFrame(information_data, columns=['information', 'value'])
 
 
-layout = Div([
+data_table = {
+    'Key' : [
+        'Satellite',
+        'Sensor',
+        'Date',
+        'Path',
+        'Row',
+        'Geo. Processing',
+        'Radio. Processing',
+        'Action',
+        '',
+    ],
+    'Value' : [
+        dcc.Dropdown(
+            id='satellite',
+            options=[
+                {'label': 'AMAZONIA1', 'value': 'AMAZONIA1'},
+                {'label': 'CBERS2B', 'value': 'CBERS2B'},
+                {'label': 'CBERS4', 'value': 'CBERS4'},
+                {'label': 'CBERS4A', 'value': 'CBERS4A'},
+                {'label': 'LANDSAT1', 'value': 'LANDSAT1'},
+                {'label': 'LANDSAT2', 'value': 'LANDSAT2'},
+                {'label': 'LANDSAT3', 'value': 'LANDSAT3'},
+                {'label': 'LANDSAT5', 'value': 'LANDSAT5'},
+                {'label': 'LANDSAT7', 'value': 'LANDSAT7'},
+            ],
+            value='CBERS2B',
+            clearable=False
+        ),
+        dcc.Dropdown(
+            id='sensor',
+            options=[
+                {'label': 'All', 'value': 'All'},
+                {'label': 'WFI', 'value': 'WFI'},
+                {'label': 'MUX', 'value': 'MUX'}
+            ],
+            value='All',
+            clearable=False
+        ),
+        dcc.DatePickerRange(
+            id='date-picker-range',
+            min_date_allowed=date(2020, 6, 1),
+            max_date_allowed=date(2020, 6, 30),
+            initial_visible_month=date(2020, 6, 1),
+            start_date=date(2020, 6, 5),
+            end_date=date(2020, 6, 14)
+        ),
+        dcc.Input(id='path', placeholder='Path', type='number', value=1, min=1, max=999),
+        dcc.Input(id='row', placeholder='Row', type='number', value=1, min=1, max=999),
+        dcc.RadioItems(
+            id='geo_processing',
+            options=[
+                {'label': 'All', 'value': 'ALL'},
+                {'label': '2', 'value': '2'},
+                {'label': '4', 'value': '4'}
+            ],
+            value='ALL',
+            labelStyle={'display': 'inline-block'}
+        ),
+        dcc.RadioItems(
+            id='radio_processing',
+            options=[
+                {'label': 'All', 'value': 'ALL'},
+                {'label': 'DN', 'value': 'DN'},
+                {'label': 'SR', 'value': 'SR'},
+            ],
+            value='ALL',
+            labelStyle={'display': 'inline-block'}
+        ),
+        dcc.Dropdown(
+            id='action',
+            options=[
+                {'label': '/publish', 'value': 'publish'},
+            ],
+            value='publish',
+            clearable=False
+        ),
+        html.Button('Submit', id='submit_button')
+    ]
+}
+
+
+def generate_table(df, max_rows=26):
+    return html.Table(
+        # Header
+        # [ Tr([Th(col) for col in df.columns]) ] +
+        # Body
+        [
+            html.Tr([
+                html.Td(df.iloc[i][col]) for col in df.columns
+            ]) for i in range(min(len(df), max_rows))
+        ],
+        style={'color': colors['text']}
+    )
+
+
+layout = html.Div([
     # title
-    H1(
+    html.H1(
         children='publisher-dash',
         style={'textAlign': 'center', 'color': colors['text']}
     ),
     # subtitle
-    H3(
+    html.H3(
         children='Operation analysis',
         style={'textAlign': 'center', 'color': colors['text']}
     ),
 
-    # information table
-    Div([
-        # table information, date picker range and limit
-        Div([
+    html.Div([
+        html.Div([
+            generate_table(DataFrame(data_table)),
+        ], style={'padding-right': '50px'}),
+        html.Div([
+            # title
+            html.P(
+                children='Table: Information',
+                style={'textAlign': 'center', 'color': colors['text']}
+            ),
             # table information
-            Div([
-                # title
-                P(
-                    children='Table: Information',
-                    style={
-                        'textAlign': 'center',
-                        'color': colors['text']
-                    }
-                ),
-                # table information
-                DataTable(
-                    id='publisher--table--information',
-                    columns=[{"name": i, "id": i} for i in df_information.columns],
-                    data=df_information.to_dict('records'),
-                    fixed_rows={'headers': True, 'data': 0},
-                    **get_table_styles()
-                ),
-            ], style={'max-width': '500px'}),
-            # limit
-            Div([
-                # limit
-                P(
-                    children='Limit (max. 1000):',
-                    style={
-                        'textAlign': 'center',
-                        'color': colors['text'],
-                        'margin-top': '20px'
-                    }
-                ),
-                # date picker range
-                dcc_Input(
-                    id="publisher--input--limit",
-                    type="number",
-                    placeholder="Limit (max. 1000)",
-                    value=100,
-                    min=1,
-                    max=1000
-                )
-            ], style={'padding': '10px'}),
-        ], style={'width': '100%', 'display': 'flex', 'align-items': 'center', 'justify-content': 'center'}),
-    ], style={'padding': '10px'})
+            DataTable(
+                id='publisher--table--information',
+                columns=[{"name": i, "id": i} for i in df_information.columns],
+                data=df_information.to_dict('records'),
+                fixed_rows={'headers': True, 'data': 0},
+                **get_table_styles()
+            )
+        ]),
+    ], style={'width': '100%', 'display': 'flex', 'align-items': 'center', 'justify-content': 'center'}),
 ])
